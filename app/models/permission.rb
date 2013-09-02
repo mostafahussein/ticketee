511d479e41f790cf55658a23da37b14ac9b2a511
code-vortex <darkbrotherhood.assassin@gmail.com>
2013-09-02 06:33:07 +0200

    Made projects only visible to users with permission to see them

---
+++ app/models/permission.rb
@@ -0,0 +1,7 @@
+class Permission < ActiveRecord::Base
+  attr_accessible :action, :thing_id, :thing_type, :user_id
+  attr_accessible :user, :thing
+  
+  belongs_to :user
+  belongs_to :thing, :polymorphic => true
+end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,13 @@
 class Project < ActiveRecord::Base
-	validates_presence_of :name
+	
 	attr_accessible :name
-	has_many :tickets, :dependent => :delete_all
-end
+
+	validates :name, :presence => true, :uniqueness => true
+	
+	has_many :permissions, :as => :thing
+	scope :readable_by, lambda { |user| 
+		joins(:permissions).where(:permissions => { :action => "view",
+			:user_id => user.id})  }
+		
+ 	has_many :tickets, :dependent => :delete_all
+	end
